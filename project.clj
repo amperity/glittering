2e7a64400b945d3d965b0a4d1214e1c2fb830f2c
Chuck Sakoda <chuck@amperity.com>
2017-06-07 13:20:08 -0700

    Setup for repro

--- project.clj
+++ project.clj
@@ -3,16 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [gorillalabs/sparkling "1.2.3"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [gorillalabs/sparkling "1.2.5"]
                  [t6/from-scala "0.2.1"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
   :aot [#".*" glittering.serialization sparkling.destructuring]
-  :profiles {:default [:base :system :user :provided :spark-1.4.0 :dev]
-             :spark-1.4.0 ^{:pom-scope :provided}
-             {:dependencies [[org.apache.spark/spark-core_2.10 "1.4.0"]
-                             [org.apache.spark/spark-graphx_2.10 "1.4.0"]]}
+  :profiles {:default [:base :system :user :provided :spark-1.6.1 :dev]
+             :spark-1.6.1 ^{:pom-scope :provided}
+             {:dependencies [[org.apache.spark/spark-core_2.10 "1.6.1"]
+                             [org.apache.spark/spark-graphx_2.10 "1.6.1"]]}
              :test {:resource-paths ["dev-resources"]
                     :aot [glittering.core
                           glittering.pregel
